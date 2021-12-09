50ba595995920c5ffc8f8ce6a3e45deb1a228bb3
Elvan H <elvanhd@gmail.com>
2021-12-09 23:58:48 +0700

    adding a game-over screen

--- app.js
+++ app.js
@@ -7,7 +7,8 @@ const app = Vue.createApp({
     return {
       playerHealth: 100,
       monsterHealth: 100,
-      currentRound: 0
+      currentRound: 0,
+      winner: null
     };
   },
 
@@ -26,6 +27,24 @@ const app = Vue.createApp({
 
     mayUseSpecialAttack() {
       return this.currentRound % 3 === 0;
+    }
+  },
+
+  watch: {
+    playerHealth(value) {
+      if (value <= 0 && this.monsterHealth <= 0) {
+        this.winner = 'draw';
+      } else if (value <= 0) {
+        this.winner = 'monster';
+      }
+    },
+
+    monsterHealth(value) {
+      if (value <= 0 && this.playerHealth <= 0) {
+        this.winner = 'draw';
+      } else if (value <= 0) {
+        this.winner = 'player';
+      }
     }
   },
 
